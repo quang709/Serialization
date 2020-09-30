fbc6c872256689e992dd77219be010be9a36eb7c
quang709 <69231857+quang709@users.noreply.github.com>
2020-09-30 11:22:28 +0700

    Serialization

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Student> students = new List<Student>
+
+            {
+              new Student ("quang", 19, 1, 60.4),
+              new Student ("quangg", 20, 2, 66.66),
+                new Student ("quanggg", 20, 3, 66.66),
+                  new Student ("quanggg", 20, 4444, 66.66),
+            };
+            string student_file = @"C:\Users\admin\source\repos\Serialization\Serialization\pxu.xml";
+            using (Stream fs = new FileStream(student_file,FileMode.Create,FileAccess.Write,FileShare.None))
+            {
+                XmlSerializer xmlSerializer = new XmlSerializer(typeof(List<Student>));
+                xmlSerializer.Serialize(fs, students);
+            }
+
+
+
+
+            List<Student> students1 = null;
+            XmlSerializer xmlSerializer1 = new XmlSerializer(typeof(List<Student>));
+
+            using (FileStream fs1 =File.OpenRead(student_file))
+            {
+                students1 = (List<Student>)xmlSerializer1.Deserialize(fs1);
+            }
+            foreach (var item in students1)
+            {
+                Console.WriteLine(item.ToString());
+            }
+
+        }
+    }
+}
+
---
+++ Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+
+namespace Serialization
+{
+    [Serializable()]
+   public class Student : ISerializable
+    {
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Id { get; set; }
+        public double Weight { get; set; }
+
+
+
+        public Student(string name, int age, int id, double weight)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Weight = weight;
+        }
+     public Student() { }
+
+
+        public override string ToString()
+        {
+            return string.Format($"sinh vien {Name} co id {Id} nam nay {Age}");
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("Id", Id);
+            info.AddValue("Name", Name);
+            info.AddValue("Age", Age);
+            info.AddValue("Weight", Weight);
+
+        }
+    }
+
+}
